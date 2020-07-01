c4dc008693a4085c6efd09bb30af945c40a14ed3
wmq <1782976211@qq.com>
2020-07-01 14:09:28 +0800

    添加compositionApi

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Autor: Wang
+ * @Date: 2020-07-01 10:52:05
+ * @LastEditors: Wang
+ * @LastEditTime: 2020-07-01 14:05:37
+ */
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import VueCompositionApi from '@vue/composition-api';
 
 Vue.config.productionTip = false;
+Vue.use(VueCompositionApi);
 
 new Vue({
   router,
